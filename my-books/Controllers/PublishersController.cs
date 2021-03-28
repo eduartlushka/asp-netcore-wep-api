b904a87cf3090cc63794d4652e76f3aaba940f3d
Eduart Lushka <eduart.lushka@gmail.com>
2021-03-29 00:23:37 +0200

    changes for relations data

---
+++ my-books/Controllers/PublishersController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using my_books.Data.Services;
+using my_books.Data.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace my_books.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PublishersController : ControllerBase
+    {
+        private PublishersServices _publishersServices;
+        public PublishersController(PublishersServices publishersServices)
+        {
+            _publishersServices = publishersServices;
+        }
+
+        [HttpPost("add-publisher")]
+        public IActionResult AddPublisher([FromBody] PublisherVM publisher)
+        {
+            _publishersServices.AddPublisher(publisher);
+            return Ok();
+        }
+
+        [HttpGet("get-publisher-books-with-authors/{id}")]
+        public IActionResult GetPublisherData(int id)
+        {
+            var publisher = _publishersServices.GetPublisherData(id);
+
+            return Ok(publisher);
+        }
+
+        [HttpDelete("delete-publisher-by-id/{id}")]
+        public IActionResult DeletePublisherById(int id)
+        {
+            _publishersServices.DeletePublisherById(id);
+            return Ok();
+        }
+    }
+}
---
+++ my-books/Data/Services/PublishersService.cs
@@ -0,0 +1,54 @@
+﻿using my_books.Data.Models;
+using my_books.Data.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace my_books.Data.Services
+{
+    public class PublishersServices
+    {
+        private AppDbContext _context;
+        public PublishersServices(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void AddPublisher(PublisherVM publisher)
+        {
+            var _publisher = new Publisher()
+            {
+                Name = publisher.Name
+            };
+
+            _context.Publishers.Add(_publisher);
+            _context.SaveChanges();
+        }
+
+        public PublisherWithBooksAndAuthorsVM GetPublisherData(int publisherId)
+        {
+            var _publisherData = _context.Publishers.Where(x => x.Id == publisherId).Select(x => new PublisherWithBooksAndAuthorsVM()
+            {
+                Name = x.Name,
+                BookAuthors = x.Books.Select(x => new BookAuthorVM()
+                {
+                    BookName = x.Title,
+                    BookAuthors = x.Book_Authors.Select(x => x.Author.FullName).ToList()
+                }).ToList()
+            }).FirstOrDefault();
+
+            return _publisherData;
+        }
+
+        public void DeletePublisherById(int id)
+        {
+            var _publisher = _context.Publishers.FirstOrDefault(x => x.Id == id);
+            if(_publisher != null)
+            {
+                _context.Publishers.Remove(_publisher);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
