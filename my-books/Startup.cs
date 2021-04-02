cc6469040b282e989be84811bf75e7485815fe47
Eduart Lushka <eduart.lushka@gmail.com>
2021-04-02 22:36:09 +0200

    versioning

--- my-books/Startup.cs
+++ my-books/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -42,6 +43,16 @@ namespace my_books
             services.AddTransient<BooksService>();
             services.AddTransient<AuthorsService>();
             services.AddTransient<PublishersServices>();
+
+            // api versioning
+            services.AddApiVersioning(config =>
+            {
+                config.DefaultApiVersion = new ApiVersion(1, 0);
+                config.AssumeDefaultVersionWhenUnspecified = true;
+
+                // config.ApiVersionReader = new HeaderApiVersionReader("custom-version-header");
+                // config.ApiVersionReader = new MediaTypeApiVersionReader();
+            });
 
             services.AddSwaggerGen(c =>
             {
