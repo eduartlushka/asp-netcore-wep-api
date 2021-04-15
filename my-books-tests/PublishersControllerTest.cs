1a42780aa252edee2922699c5e9be41daf8b8464
Eduart Lushka <eduart.lushka@gmail.com>
2021-04-15 23:28:25 +0200

    other unit test for publisher controller

--- my-books-tests/PublishersControllerTest.cs
+++ my-books-tests/PublishersControllerTest.cs
@@ -5,6 +5,7 @@ using my_books.Controllers;
 using my_books.Data;
 using my_books.Data.Models;
 using my_books.Data.Services;
+using my_books.Data.ViewModels;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
@@ -60,6 +61,72 @@ namespace my_books_tests
             Assert.That(actionResultSecondPageData.First().Name, Is.EqualTo("Publisher 4"));
             Assert.That(actionResultSecondPageData.First().Id, Is.EqualTo(4));
             Assert.That(actionResultSecondPageData.Count, Is.EqualTo(2));
+        }
+
+        [Test, Order(2)]
+        public void HTTPGET_GetPublisherById_ReturnOk_Test()
+        {
+            int publisherId = 2;
+            IActionResult actionResult = publishersController.GetPublisherById(publisherId);
+
+            Assert.That(actionResult, Is.TypeOf<OkObjectResult>());
+
+            var publisherData = (actionResult as OkObjectResult).Value as Publisher;
+            Assert.That(publisherData.Id, Is.EqualTo(2));
+            Assert.That(publisherData.Name, Is.EqualTo("publisher 2").IgnoreCase);
+        }
+
+        [Test, Order(3)]
+        public void HTTPGET_GetPublisherById_ReturnNoFound_Test()
+        {
+            int publisherId = 99;
+            IActionResult actionResult = publishersController.GetPublisherById(publisherId);
+
+            Assert.That(actionResult, Is.TypeOf<NotFoundResult>());
+        }
+
+        [Test, Order(4)]
+        public void HTTPPOST_AddPublisher_ReturnsCreated_Test()
+        {
+            var newPunlisherVM = new PublisherVM()
+            {
+                Name = "New Publisher"
+            };
+
+            IActionResult actionResult = publishersController.AddPublisher(newPunlisherVM);
+
+            Assert.That(actionResult, Is.TypeOf<CreatedResult>());
+        }
+
+        [Test, Order(5)]
+        public void HTTPPOST_AddPublisher_ReturnsBadRequest_Test()
+        {
+            var newPunlisherVM = new PublisherVM()
+            {
+                Name = "123 New Publisher"
+            };
+
+            IActionResult actionResult = publishersController.AddPublisher(newPunlisherVM);
+
+            Assert.That(actionResult, Is.TypeOf<BadRequestObjectResult>());
+        }
+
+        [Test, Order(6)]
+        public void HTTPDELETE_DeletePublisherById_ReturnsOk_Test()
+        {
+            int publisherId = 6;
+            IActionResult actionResult = publishersController.DeletePublisherById(publisherId);
+
+            Assert.That(actionResult, Is.TypeOf<OkResult>());
+        }
+
+        [Test, Order(7)]
+        public void HTTPDELETE_DeletePublisherById_ReturnsBadRequest_Test()
+        {
+            int publisherId = 6;
+            IActionResult actionResult = publishersController.DeletePublisherById(publisherId);
+
+            Assert.That(actionResult, Is.TypeOf<BadRequestObjectResult>());
         }
 
         [OneTimeTearDown]
